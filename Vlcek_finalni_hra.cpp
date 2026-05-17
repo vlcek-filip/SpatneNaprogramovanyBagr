c276e4e5fae9bc83b363470b254275eef706cd43
vlcek-filip <fiila.fik@gmail.com>
2026-05-17 20:45:06 +0200

    Create Vlcek_finalni_hra.cpp

---
+++ Vlcek_finalni_hra.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int zivoty = 100;
+int maxzivoty;
+int zlato;
+int mana;
+int maxmana;
+string class;
+
+
+int main(){
+
+cout << "-----------VITEJ VE HRE-----------" << endl;
+cout << "Za jakou tridu budete chtit hrat? (1 - Paladin, 2 - Lovec, Mag - 3, Warlock - 4)" << endl;
+cin >> class; 
+
+} 
