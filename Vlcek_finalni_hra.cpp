9cd974fe7047e5099e0a9f7b3c07c075d677d34d
vlcek-filip <fiila.fik@gmail.com>
2026-05-18 21:46:21 +0200

    Auto commit

--- Vlcek_finalni_hra.cpp
+++ Vlcek_finalni_hra.cpp
@@ -14,80 +14,9 @@ int vyber;
 int akce;
 string pokracakce;
 
-int main(){
 
-cout << "-=-=-=-=-=-VITEJ VE HRE-=-=-=-=-=-" << endl;
-// POZNAMKA: nevedel jak jsem mel dat ze kdyz vyber nema hodnotu, tak aby ten cyklus bezel dokud int vyber nebude mit prirazenou hodnotu, zeptal jsem se na to AI.
-while (!vyber){
-cout << "Za jakou tridu budete chtit hrat? (Paladin, Lovec, Mag, Warlock): ";
-cin >> classa;
-cout << endl;
-
-
-if (classa == "Paladin"){
-    cout << "Vas vyber: " << classa << endl;
-        maxzivoty = 5;
-    cout << "----------------------" << endl;
-    cout << "zivoty = 5/5" << endl;
-        zivoty = 5;
-    cout << "utok = 3" << endl;
-        utok = 3;
-    cout << "mana = 5" << endl;
-        mana = 5;
-    cout << "----------------------" << endl;
-
-    }else if (classa == "Lovec"){
-    cout << "Vas vyber: " << classa << endl;
-        maxzivoty = 4;
-    cout << "----------------------" << endl;
-    cout << "zivoty = 4/4" << endl;
-        zivoty = 4;
-    cout << "utok = 3" << endl;
-        utok = 4;
-    cout << "mana = 2" << endl;
-        mana = 2;
-    cout << "----------------------" << endl;
-
-    }else if (classa == "Mag"){
-    cout << "Vas vyber: " << classa << endl;
-       maxzivoty = 3;
-    cout << "----------------------" << endl;
-    cout << "zivoty = 3/3" << endl;
-       zivoty = 3;
-    cout << "utok = 3" << endl;
-       utok = 4;
-    cout << "mana = 10" << endl;
-       mana = 10;
-    cout << "----------------------" << endl;
-
-    }else if (classa == "Warlock"){
-    cout << "Vas vyber: " << classa << endl;
-        maxzivoty = 7;
-    cout << "----------------------" << endl;
-    cout << "zivoty = 7/7" << endl;
-        zivoty = 7;
-    cout << "utok = 4" << endl;
-        utok = 4;
-    cout << "mana = 1" << endl;
-        mana = 1;
-    cout << "----------------------" << endl;
-
-
-    }else{
-        cout << "neplatny vyber, vyberte znovu" << endl;
-    }
-
-    cout << "chcete tuto class vybrat? ";
-    cin >> volbaclassy;
-
-    if (volbaclassy == "ano"){
-        cout << "Class " << classa << " vybrana" << endl;
-        vyber = 1;
-        cout << "------------------------------------------------------------------" << endl;
-    }
-}
-
-
+// VESNICE
+Void V(){
 cout << endl;
 cout << "----------------------" << endl;
 cout << "Lokace: Vesnice" << endl;
@@ -172,5 +101,127 @@ if (akce == 1){
         cin >> akce;
     }
 }
+cout << "----------------------" << endl;
+}
+// ------------------------------------------------------------------
+// MONSTRUM
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+
+cout << "-=-=-=-=-=-VITEJ VE HRE-=-=-=-=-=-" << endl;
+// POZNAMKA: nevedel jak jsem mel dat ze kdyz vyber nema hodnotu, tak aby ten cyklus bezel dokud int vyber nebude mit prirazenou hodnotu, zeptal jsem se na to AI.
+while (!vyber){
+cout << "Za jakou tridu budete chtit hrat? (Paladin, Lovec, Mag, Warlock): ";
+cin >> classa;
+cout << endl;
+
+
+if (classa == "Paladin"){
+    cout << "Vas vyber: " << classa << endl;
+        maxzivoty = 5;
+    cout << "----------------------" << endl;
+    cout << "zivoty = 5/5" << endl;
+        zivoty = 5;
+    cout << "utok = 3" << endl;
+        utok = 3;
+    cout << "mana = 5" << endl;
+        mana = 5;
+    cout << "----------------------" << endl;
+
+    }else if (classa == "Lovec"){
+    cout << "Vas vyber: " << classa << endl;
+        maxzivoty = 4;
+    cout << "----------------------" << endl;
+    cout << "zivoty = 4/4" << endl;
+        zivoty = 4;
+    cout << "utok = 3" << endl;
+        utok = 4;
+    cout << "mana = 2" << endl;
+        mana = 2;
+    cout << "----------------------" << endl;
+
+    }else if (classa == "Mag"){
+    cout << "Vas vyber: " << classa << endl;
+       maxzivoty = 3;
+    cout << "----------------------" << endl;
+    cout << "zivoty = 3/3" << endl;
+       zivoty = 3;
+    cout << "utok = 3" << endl;
+       utok = 4;
+    cout << "mana = 10" << endl;
+       mana = 10;
+    cout << "----------------------" << endl;
+
+    }else if (classa == "Warlock"){
+    cout << "Vas vyber: " << classa << endl;
+        maxzivoty = 7;
+    cout << "----------------------" << endl;
+    cout << "zivoty = 7/7" << endl;
+        zivoty = 7;
+    cout << "utok = 4" << endl;
+        utok = 4;
+    cout << "mana = 1" << endl;
+        mana = 1;
+    cout << "----------------------" << endl;
+
+
+    }else{
+        cout << "neplatny vyber, vyberte znovu" << endl;
+    }
+
+    cout << "chcete tuto class vybrat? ";
+    cin >> volbaclassy;
+
+    if (volbaclassy == "ano"){
+        cout << "Class " << classa << " vybrana" << endl;
+        vyber = 1;
+        cout << "------------------------------------------------------------------" << endl;
+    }
+}
+
+
+V();
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
