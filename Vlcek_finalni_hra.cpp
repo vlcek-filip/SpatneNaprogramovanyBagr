a7d8d60e12dd420463a2469c53c606951319be74
vlcek-filip <fiila.fik@gmail.com>
2026-05-18 15:17:11 +0200

    Auto commit

--- Vlcek_finalni_hra.cpp
+++ Vlcek_finalni_hra.cpp
@@ -2,19 +2,45 @@
 
 using namespace std;
 
-int zivoty = 100;
+int zivoty;
 int maxzivoty;
 int zlato;
 int mana;
 int maxmana;
 string class;
 
-// TEST 696768
 
 int main(){
 
 cout << "-----------VITEJ VE HRE-----------" << endl;
-cout << "Za jakou tridu budete chtit hrat? (1 - Paladin, 2 - Lovec, Mag - 3, Warlock - 4)" << endl;
+cout << "Za jakou tridu budete chtit hrat? (Paladin, Lovec, Mag, Warlock)" << endl;
 cin >> class; 
 
+if (class == 'Paladin'){
+        maxzivoty = 5;
+        zivoty = 5;
+        utok = 3;
+        mana = 5;
+
+    }else if (class == 'Lovec'){
+        maxzivoty = 4;
+        zivoty = 4;
+        utok = 4;
+        mana = 2;
+
+    }else if (class == 'Mag'){
+       maxzivoty = 3;
+       zivoty = 3;
+       utok = 4;
+       mana = 10;
+
+    }else if (class == 'Warlock'){
+        maxzivoty = 7;
+        zivoty = 7;
+        utok = 4;
+        mana = 1;
+
+
+
+    }
 } 
